dbed848acb5dd7374d0577aa63704e0e4afc9cdd
KUGOUNagi <e8896@g.maizuru-ct.ac.jp>
2022-09-05 15:17:28 +0900

    shift-jisをuft-8に変換する(途中)

--- uft.py
+++ uft.py
@@ -1,7 +1,8 @@
 import codecs
+import sys
 
-fin = codecs.open("untitled.txt", "r", "uft-8")
-fout_utf = codecs.open("utf-8.txt", "w", "shift_jis")
+fin = codecs.open("untitled.txt", "r", "shift_jis")
+fout_utf = codecs.open("utf-8.txt", "w", "uft-8")
 for row in fin:
     fout_utf.write(row)
 fin.close()
